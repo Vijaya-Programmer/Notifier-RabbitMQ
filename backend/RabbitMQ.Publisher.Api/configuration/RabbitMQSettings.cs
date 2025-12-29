beda9edc1e50cb4bbaee07906cf40b1b6bf0fc1d
Baskaran Vijayalakshmi <vijayastack2022@gmail.com>
2025-12-29 08:46:55 +0800

    added rabbitMQSettings

---
+++ backend/RabbitMQ.Publisher.Api/configuration/RabbitMQSettings.cs
@@ -0,0 +1,9 @@
+namespace RabbitMQ.publisher.Api.Configuration
+{
+  public class RabbitMQSettings
+  {
+    public string HostName { get; set; } = "localhost";
+    public string UserName { get; set; } = "guest";
+    public string Password { get; set; } = "guest";
+  }
+}
